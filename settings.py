e79172b54e50237ac83de5ecb30b61a57c865ef9
darren277 <darren277@yahoo.com>
2025-09-29 16:14:36 -0700

    Notion DB ID env var.

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 NOTION_API_TOKEN = os.environ.get("NOTION_API_TOKEN")
-NOTION_DATABASE_ID = "YOUR_NOTION_DATABASE_ID" # <-- Put your Notion Database ID here
+NOTION_DATABASE_ID = os.environ.get("NOTION_DATABASE_ID")
 CHROMA_DB_PATH = "./notion_db" # Path to store ChromaDB data
 CHROMA_COLLECTION_NAME = "notion-knowledge-base"
 
