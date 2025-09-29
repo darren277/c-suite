23ba272fec87219e5ea7268db7b782c28ec0254f
darren277 <darren277@yahoo.com>
2025-09-29 16:14:09 -0700

    Rich text processing.

--- lib/sync_notion.py
+++ lib/sync_notion.py
@@ -17,8 +17,56 @@ def get_text_from_blocks(blocks):
     """Extracts plain text from a list of Notion block objects."""
     text = []
     for block in blocks:
-        if block['type'] == 'paragraph':
-            text.append(block['paragraph']['rich_text'][0]['plain_text'])
+        block_type = block.get('type', '')
+        
+        if block_type == 'paragraph':
+            rich_text = block.get('paragraph', {}).get('rich_text', [])
+            if rich_text:
+                text.append(rich_text[0].get('plain_text', ''))
+        
+        elif block_type == 'heading_1':
+            rich_text = block.get('heading_1', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"# {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'heading_2':
+            rich_text = block.get('heading_2', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"## {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'heading_3':
+            rich_text = block.get('heading_3', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"### {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'bulleted_list_item':
+            rich_text = block.get('bulleted_list_item', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"• {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'numbered_list_item':
+            rich_text = block.get('numbered_list_item', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"1. {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'to_do':
+            rich_text = block.get('to_do', {}).get('rich_text', [])
+            checked = block.get('to_do', {}).get('checked', False)
+            if rich_text:
+                checkbox = "☑" if checked else "☐"
+                text.append(f"{checkbox} {rich_text[0].get('plain_text', '')}")
+        
+        elif block_type == 'code':
+            rich_text = block.get('code', {}).get('rich_text', [])
+            language = block.get('code', {}).get('language', '')
+            if rich_text:
+                text.append(f"```{language}\n{rich_text[0].get('plain_text', '')}\n```")
+        
+        elif block_type == 'quote':
+            rich_text = block.get('quote', {}).get('rich_text', [])
+            if rich_text:
+                text.append(f"> {rich_text[0].get('plain_text', '')}")
+    
     return "\n".join(text)
 
 def chunk_text(text, chunk_size=1000):
